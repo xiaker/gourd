e77d5885e4d24365fc369af53349edbd210f0cd4
fatrbaby <fatrbaby@qq.com>
2017-02-13 15:49:21 +0800

    1st blood

--- src/Container.php
+++ src/Container.php
@@ -1,11 +1,61 @@
 <?php
 
+namespace Xiaker\Gourd;
+
+use \Exception;
+
 class Container implements \ArrayAccess
 {
+    protected $storage = [];
     protected $factories;
 
     public function __construct()
     {
         $this->factories = new \SplObjectStrorage();
+    }
+
+    public function get($name)
+    {
+
+    }
+
+    public function instance($name, $instance)
+    {
+        $this->storage[$name] = $instance;
+    }
+
+    public function singleton($name, $concrete)
+    {
+        if (isset($this->storage[$name])) {
+            throw new Exception('Can not reload Object.');
+        }
+
+        return $this->storage[$name] = $concrete;
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->storage[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        if ($this->storage[$offset]) {
+            return $this->storage[$offset];
+        }
+
+        throw new Exception('Undefined offset of Container.');
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->storage[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if (isset($this->storage[$offset])) {
+            unset($this->storage[$offset]);
+        }
     }
 }
---
+++ test/ContainerTest.php
@@ -0,0 +1,6 @@
+<?php
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
