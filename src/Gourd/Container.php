48020b18d4fe6f78093971ea573d7e05d0a77348
fatrbaby <fatrbaby@qq.com>
2017-04-01 17:07:27 +0800

    (features)basic features

--- src/Gourd/Container.php
+++ src/Gourd/Container.php
@@ -2,20 +2,79 @@
 
 namespace Xiaker\Gourd;
 
+use ArrayAccess;
+use Countable;
+use Closure;
 use Psr\Container\ContainerInterface;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 
-class Container implements ContainerInterface
+class Container implements ContainerInterface, ArrayAccess, Countable
 {
-    protected $
+    protected $bindings = [];
+
     public function get($id)
     {
-        // TODO: Implement get() method.
+        if (!$this->has($id)) {
+            throw new NotFoundException();
+        }
+
+        $raw = $this->bindings[$id];
+
+        if ($raw instanceof Closure) {
+            return $this->call($raw);
+        }
+
+        return $this->make($raw);
     }
 
     public function has($id)
     {
-        // TODO: Implement has() method.
+        return $this->offsetExists($id);
+    }
+
+    public function call(Closure $id)
+    {
+        // todo
+    }
+
+    public function make($id, $singleton = true)
+    {
+        // todo
+    }
+
+    public function offsetGet($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            return $this->bindings[$offset];
+        }
+
+        throw new NotFoundException();
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->bindings[$offset]);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if ($this->offsetExists($offset)) {
+            throw new ContainerException('Offset always ready exists.');
+        }
+
+        $this->bindings[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            unset($this->bindings[$offset]);
+        }
+    }
+
+    public function count()
+    {
+        return count($this->bindings);
     }
 }
